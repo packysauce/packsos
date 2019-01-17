c5bf907457716ee7940edae45bcbd8bee9020789
Patrick White <patrick@patrickwhite.org>
2019-01-17 08:08:15 -0800

    print page tables

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 #![cfg_attr(not(test), no_main)]
 #![cfg_attr(test, allow(unused_imports))]
 
-use acpi::search_for_rsdp_bios;
 use core::panic::PanicInfo;
 use packsos::{println, serial_print, serial_println};
 use x86_64::instructions::interrupts;
@@ -20,9 +19,16 @@ pub extern "C" fn _start() -> ! {
     packsos::gdt::init();
     packsos::interrupts::init_idt();
     unsafe { packsos::interrupts::PIC.lock().initialize() };
+    let page_table = 0xffff_ffff_ffff_f000 as *const u64;
     interrupts::enable();
 
     serial_println!("Smash circuits online");
+    for i in 0..512 {
+        let entry = unsafe { *page_table.offset(i) };
+        if entry != 0 {
+            serial_println!("Entry {:3}: {:#x}", i, entry);
+        }
+    }
     println!("Lemme smash");
     println!("You want some blue?");
     packsos::halt();
